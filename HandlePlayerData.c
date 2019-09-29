86f7a3157c483800ad7cc291f630245eb128458b
EricMendez <100findme@gmail.com>
2019-09-29 18:27:13 -0400

    Add end casting output logic
    Have handler functions that are called when the casting ends. I'm trying to implement them asynchronously because they do rely on timing, but I don't want to use delays / slow down the code

--- HandlePlayerData.c
+++ HandlePlayerData.c
@@ -4,7 +4,6 @@
  *
  */
 
-#include <stdio.h>
 #include <PlayerData.h>
 
 // -----------------------------------------------
@@ -18,7 +17,10 @@ PlayerData* initPlayerStruct(int[] spellsChosen) {
 		.isCasting = false,
 		.isRumbling = false,
 		.rumbleLevel = false,
+		.rumbleStartTime = 0,
 		.isLit = false,
+		.lightLevel = 0,
+		.lightStartTime = 0,
 		.isLoud = false,
 		.keyOfSoundToPlay = -1,
 		.isShielding = false,
@@ -41,6 +43,12 @@ void rumbler(PlayerData* P) {
 	/* Case for spell start */
 	if(P->isRumbling && P->isCasting) {
 		P->rumbleLevel = SPELL_CASTING_RUMBLE_LEVEL;
+		P->rumbleStartTime = clock();
+	}
+	/* Case for spell end */
+	else if(P->isRumbling && !(P->isCasting)) {
+		P->rumbleLevel = SPELL_END_RUMBLE_LEVEL;
+		P->rumbleStartTime = clock();
 	}
 }
 
@@ -50,9 +58,17 @@ void rumbler(PlayerData* P) {
  *				< End goal -> make light pulse slowly >
  */
 void lights(PlayerData* P) {
-	/* Case for spell start */
+	/* Case for spell start - TODO: Add light sequence */
 	if(P->isLit && P->isCasting) {
-		P->litLevel = SPELL_CASTING_LIGHT_LEVEL; /* placeholder for now */
+		P->lightLevel = SPELL_CASTING_LIGHT_LEVEL; /* placeholder for now */
+		P->lightStartTime = clock();
+	}
+	/* Case for spell end */
+	else if(P->isLit && !(P->isCasting)) {
+		/* stop light for now to signal done with spell
+		 * TODO: Add light sequence */
+		P->lightLevel = SPELL_END_LIGHT_LEVEL;
+		P->lightStartTime = clock();
 	}
 }
 
@@ -81,7 +97,7 @@ void changeSpell(PlayerData* P, int direction) {
 	}
 }
 
-/* Button pressed to initialize cast sequence
+/* Button was pressed to initialize cast sequence
  */
 void startCasting(PlayerData* P) {
 	P->isCasting = true;
@@ -92,4 +108,46 @@ void startCasting(PlayerData* P) {
 
 	P->isLit = true;
 	lights(P);
+}
+
+/* Pressure sensor was pressed end cast sequence
+ */
+void endCasting(PlayerData* P) {
+	P->isCasting = false;
+	/* Do one last rumble and LED flash to show the spell is completed and sent
+	 * over to the other player */
+	rumbler(P);
+	lights(P);
+	P->isRumbling = false; /* 1 more rumble after as the spell gets sent out */
+	P->isLit = false; /* 1 more light flash after as the spell get sent out */
+}
+
+// -----------------------------------------------
+// Update functions that rely on timing
+// -----------------------------------------------
+
+/*
+ * Stop rumbling after set time - which is in player data
+ * need to keep calling in main function to keep track of the clock cycles
+ * and to to stop the rumbling at appropriate times
+ *
+ * Rumble stopping events:
+ *  - Stop rumbling 1.5 sec after a spell has been cast and completed
+ */
+void stopRumbler(PlayerData* P) {
+	/* Stop the rumble after last rumble from sending the spell out */
+	if(!(P->isCasting) && !(P->isRumbling) &&
+		(clock() - P->rumbleStartTime >= END_SPELL_RUMBLE_TIME)) {
+		P->rumbleLevel = 0;
+		P->rumbleStartTime = 0; /* technically don't need to change this, but
+								   just to be verbose I'm doing it anyways */
+	}
+}
+
+void stopLights(PlayerData* P) {
+	/* Stop the lights gradually */
+	if(!(P->isCasting) && !(P->isLit)) {
+		/* TODO: Create a function that increases then decreases the light
+		level so that it looks like a pulse */
+	}
 }
--- PlayerData.h
+++ PlayerData.h
@@ -3,12 +3,23 @@
  * Dictionaries with spell types and sounds to play
  */
 
+#include <studio.h>
+#include <time.h>
+
 #ifndef HEADER_GAURD_FOR_PLAYER_DATA
 #define HEADER_GAURD_FOR_PLAYER_DATA
 
 #define NUM_SPELLS_ALLOWED = 3;
+
 #define SPELL_CASTING_RUMBLE_LEVEL = 20; /* out of 100 */
+#define SPELL_END_RUMBLE_LEVEL = 70; /* out of 100 */
+
 #define SPELL_CASTING_LIGHT_LEVEL = 20; /* out of 100 */
+#define SPELL_END_LIGHT_LEVEL = 70; /* out of 100 */
+
+/* 4,000,000,000 clocks = 1 sec ??-> TODO: need to confirm number with tests */
+#define END_SPELL_RUMBLE_TIME = 4000000000 * 1.5;
+#define END_SPELL_LIGHT_TIME = 4000000000 * 1.5;
 
 /* spells allowed in staff inventory */
 const size_t numSpellsAllowed = NUM_SPELLS_ALLOWED;
@@ -27,9 +38,11 @@ typedef struct PlayerStaffData
 
 	bool isRumbling;
 	size_t rumbleLevel; /* 0 -> 100 */
+	clock_t rumbleStartTime;
 
 	bool isLit;
-	size_t litLevel; /* 0 -> 100 */
+	size_t lightLevel; /* 0 -> 100 */
+	clock_t lightStartTime;
 
 	bool isLoud;
 	int keyOfSoundToPlay; /* if none -> -1, else -> key for soundBook */
@@ -41,7 +54,7 @@ typedef struct PlayerStaffData
 } PlayerData;
 
 /* each index of the array equates to it's spell ID */
-const char* bookOfSpells[numSpellsInBook] = [
+const char *bookOfSpells[numSpellsInBook] = [
 	"water_defense", // 0
 	"fireball", // 1
 	"air_rush", // 2
@@ -49,7 +62,7 @@ const char* bookOfSpells[numSpellsInBook] = [
 	"splash" // 4
 ];
 
-const char* bookOfSounds[numOfSounds] = [
+const char *bookOfSounds[numOfSounds] = [
 	"sent_spell",
 	"got_hit"
 ]; /* probably have these correspond with audio files */
