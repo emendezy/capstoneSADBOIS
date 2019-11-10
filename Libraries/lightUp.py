83f01a804c92a1db0bfa5176bf42485e8c8e4e1c
EricMendez <100findme@gmail.com>
2019-11-10 17:22:29 -0500

    Update lightUp.py

--- Libraries/lightUp.py
+++ Libraries/lightUp.py
@@ -49,33 +49,29 @@ def rainbow_cycle(wait):
         pixels.show()
         time.sleep(wait)
 
-count = 0
+# # Comment this line out if you have RGBW/GRBW NeoPixels
+pixels.fill((255, 0, 0))
+# # Uncomment this line if you have RGBW/GRBW NeoPixels
+# # pixels.fill((255, 0, 0, 0))
+# pixels.show()
+# time.sleep(1)
 
-while (count <= 1000):
-    # # Comment this line out if you have RGBW/GRBW NeoPixels
-    # pixels.fill((255, 0, 0))
-    # # Uncomment this line if you have RGBW/GRBW NeoPixels
-    # # pixels.fill((255, 0, 0, 0))
-    # pixels.show()
-    # time.sleep(1)
+# # Comment this line out if you have RGBW/GRBW NeoPixels
+# pixels.fill((0, 255, 0))
+# # Uncomment this line if you have RGBW/GRBW NeoPixels
+# # pixels.fill((0, 255, 0, 0))
+# pixels.show()
+# time.sleep(1)
 
-    # # Comment this line out if you have RGBW/GRBW NeoPixels
-    # pixels.fill((0, 255, 0))
-    # # Uncomment this line if you have RGBW/GRBW NeoPixels
-    # # pixels.fill((0, 255, 0, 0))
-    # pixels.show()
-    # time.sleep(1)
+# # Comment this line out if you have RGBW/GRBW NeoPixels
+# pixels.fill((0, 0, 255))
+# # Uncomment this line if you have RGBW/GRBW NeoPixels
+# # pixels.fill((0, 0, 255, 0))
+# pixels.show()
+# time.sleep(1)
 
-    # # Comment this line out if you have RGBW/GRBW NeoPixels
-    # pixels.fill((0, 0, 255))
-    # # Uncomment this line if you have RGBW/GRBW NeoPixels
-    # # pixels.fill((0, 0, 255, 0))
-    # pixels.show()
-    # time.sleep(1)
+# rainbow_cycle(0.001)    # rainbow cycle with 1ms delay per step
 
-    rainbow_cycle(0.001)    # rainbow cycle with 1ms delay per step
-
-    count += 1
 
 exit()
 
