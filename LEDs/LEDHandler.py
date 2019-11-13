617e51dabcd74cf400ef2660200c3e9a3a34e8a3
EricMendez <100findme@gmail.com>
2019-11-13 12:00:55 -0500

    Create LEDHandler.py

---
+++ LEDs/LEDHandler.py
@@ -0,0 +1,8 @@
+# LED strip Handler
+
+"""
+Command to run lights example:
+
+sudo PYTHONPATH="/home/pi/Desktop/rpi_ws281x/python/.:build/lib.linux-armv7l-2.7" python /home/pi/Desktop/rpi_ws281x/python/examples/strandtest.py -c
+
+"""
