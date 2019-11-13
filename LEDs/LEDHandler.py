c172b157ba5e13784205aa159f5e3e68524cc9b3
EricMendez <100findme@gmail.com>
2019-11-13 13:32:35 -0500

    Update LEDHandler.py

--- LEDs/LEDHandler.py
+++ LEDs/LEDHandler.py
@@ -5,4 +5,90 @@ Command to run lights example:
 
 sudo PYTHONPATH="/home/pi/Desktop/rpi_ws281x/python/.:build/lib.linux-armv7l-2.7" python /home/pi/Desktop/rpi_ws281x/python/examples/strandtest.py -c
 
+Command to run this led handler:
+
+sudo PYTHONPATH="/home/pi/Desktop/rpi_ws281x/python/.:build/lib.linux-armv7l-2.7" python /home/pi/Desktop/capstoneSADBOIS/LEDs/LEDHandler.py -c
+
 """
+import time
+from neopixel import *
+import argparse
+
+# LED strip configuration:
+LED_COUNT         = 30      # num of LED pixels
+LEDS_PER_SECTION  = 6       # num of leds in each cooldown strip
+LED_PIN           = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+LED_FREQ_HZ       = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA           = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS    = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT        = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL       = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+# LED index bounds for different cooldowns
+HEALING_LED_START    = 0
+BURNING_LED_START    = 6
+CRACKLING_LED_START  = 12
+BASTION_LED_START    = 18
+SECOND_LED_START     = 24
+
+#######################################
+# Define some LED fns
+
+def updateCooldown(strip, start, color, numLit):
+	for i in range(start, start + numLit):
+		strip.setPixelColor(i, color)
+        strip.show()
+
+
+if __name__ == '__main__':
+    # Process arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    parser.add_argument('-spell', dest='spellType', required=True,
+        help='this is the spellType string')
+    parser.add_argument('-lights', dest='numberOfLights', required=True,
+    	type=int, help='this represents the number of lights on out of 6')
+    args = parser.parse_args()
+
+    # Create NeoPixel object with appropriate configuration.
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+
+    print ('Press Ctrl-C to quit.')
+    if not args.clear:
+        print('Use "-c" argument to clear LEDs on exit')
+
+    try:
+    	if args.spellType == 'healing_ward':
+    		updateCooldown(strip, HEALING_LED_START, Color(255, 77, 255), args.numberOfLights)
+
+		elif args.spellType == 'burning_brand':
+			updateCooldown(strip, BURNING_LED_START, Color(255, 0, 0), args.numberOfLights)
+
+		elif args.spellType == 'crackling_bolt':
+            updateCooldown(strip, CRACKLING_LED_START, Color(255, 255, 0), args.numberOfLights)
+
+         elif args.spellType == 'bastion':
+            updateCooldown(strip, BASTION_LED_START, Color(179, 89, 0), args.numberOfLights)
+
+         elif args.spellType == 'second_wind':
+            updateCooldown(strip, SECOND_LED_START, Color(255, 255, 255), args.numberOfLights)
+
+        # while True:
+            # print ('Color wipe animations.')
+            # colorWipe(strip, Color(255, 0, 0))  # Red wipe
+            # colorWipe(strip, Color(0, 255, 0))  # Blue wipe
+            # colorWipe(strip, Color(0, 0, 255))  # Green wipe
+            # print ('Theater chase animations.')
+            # theaterChase(strip, Color(127, 127, 127))  # White theater chase
+            # theaterChase(strip, Color(127,   0,   0))  # Red theater chase
+            # theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
+            # print ('Rainbow animations.')
+            # rainbow(strip)
+            # rainbowCycle(strip)
+            # theaterChaseRainbow(strip)
+
+    except KeyboardInterrupt:
+        if args.clear:
+            colorWipe(strip, Color(0,0,0), 10)
