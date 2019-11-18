44f20f73613594ec082f186571b119ee5d27acbe
EricMendez <100findme@gmail.com>
2019-11-18 12:26:57 -0500

    Update HandlePlayerData.c

--- HandlePlayerData.c
+++ HandlePlayerData.c
@@ -333,7 +333,7 @@ void editCoolDownValues(struct PlayerStaffData* P, int amount)
 		{
 			P->coolDownMask[i] -= amount;
 		}
-		if(P->coolDownMask[i] % 5 == 0)
+		if(P->coolDownMask[i] % 5 == 0 && P->coolDownMask[i] != 0)
 		{
 			updateCooldownLightsOnStaff((30 - P->coolDownMask[i]) / 5, i);
 		}
