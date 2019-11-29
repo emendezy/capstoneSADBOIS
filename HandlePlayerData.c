334650b65ce2f776e160c342e201ca0619a6ad51
EricMendez <100findme@gmail.com>
2019-11-29 12:12:57 -0500

    try with no '&'

--- HandlePlayerData.c
+++ HandlePlayerData.c
@@ -661,7 +661,7 @@ void soundHandler(struct PlayerStaffData* P, int soundType)
 	// nohup omxplayer -o local ring.wav <&- >&- 2>&- & disown
 	// pid_t pid;
 	char* omxplayer = "omxplayer --no-keys -o local ";
-	char* end = " &";
+	// char* end = " &";
 
 	char* cmd = malloc(sizeof(omxplayer) + sizeof(bookOfSounds[soundType]) + sizeof(end));
 	cmd = strcpy(cmd, omxplayer);
