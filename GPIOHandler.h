c73ab0352dc94200316f5fb6b758bc5f0c711d74
EricMendez <100findme@gmail.com>
2019-12-09 14:55:26 -0500

    Update GPIOHandler.h

--- GPIOHandler.h
+++ GPIOHandler.h
@@ -13,14 +13,14 @@
 #include <softPwm.h>
 
 #define GAME_START_BUTTON 22
-
-#define LED_PIN 15
 #define SPELL_START_BUTTON 23
 #define SPELL_END_BUTTON 4
-
 #define RUMBLER_PIN 17
+
+
 #define MAX_ANALOG_RANGE 100
 #define ANALOG_STEP 5
+#define LED_PIN 15
 
 void initializeRaspberryPi();
 
